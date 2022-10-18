dcee2dde987ac5ac1d44144b6a7493a5aa472b7c
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2022-10-17 21:48:35 -0400

    Refactored line adjuster

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,10 @@ public class Main implements Runnable {
     public int screenWidth = 700;
     public int screenHeight = 700;
 
-    public Line line;
+    public Line modelLine;
     public Line initialLine;
     public Line adjustedLine;
+    public ArrayList<Line> adjustedLines;
 
     public static void main(String[] args){
         Main ex = new Main();
@@ -26,15 +27,10 @@ public class Main implements Runnable {
         setUpGraphics();
 
         System.out.println("Main started");
-        line = new Line(2, 7);
-        line.calculatePoints(-400, 400, 2500);
-        line.calculateErrorPoints(500, false);
+        modelLine = new Line(2, 7);
+        modelLine.calculatePoints(-400, 400, 2500);
+        modelLine.calculateErrorPoints(500, false);
         System.out.println("Calculated main line");
-
-        // Calculate deviations for (+) increment and (-) increment
-        // Choose whichever increment results in a lower deviation
-        // Repeat until both (+) increment and (-) increment result in higher deviations -> break from loop
-        // Proceed with y-intercept
 
         double slope = Math.random()*4;
         double yIntercept = Math.random()*100;
@@ -42,17 +38,38 @@ public class Main implements Runnable {
         initialLine = new Line(slope, yIntercept);
         initialLine.calculatePoints(-400, 400, 2000);
 
-        adjustedLine = new Line (slope, yIntercept);
-        adjustedLine.calculateAverageDeviation(line.errorPoints);
-
-        for (int i = 0; i < 10; i++){
-            adjustedLine.adjustSlope(line.errorPoints, 0.1);
-            adjustedLine.adjustYIntercept(line.errorPoints, 0.1);
-        }
-
+        adjustedLine = calculateAdjustedLines(slope, yIntercept, 10);
         adjustedLine.calculatePoints(-400, 400, 1000);
 
     }
+
+    public Line calculateAdjustedLines(double slope, double yIntercept, int iterations){
+
+        double bestSlope = 0;
+        double bestYIntercept = 0;
+        double bestAverageDeviation = 0;
+
+        for (int i = 0; i < iterations; i++){
+
+            Line testLine = new Line(slope, yIntercept);
+            testLine.adjustSlope(modelLine.errorPoints, 0.1);
+            testLine.adjustYIntercept(modelLine.errorPoints, 0.1);
+            testLine.calculateAverageDeviation(modelLine.errorPoints);
+
+            if (i == 0 || testLine.averageDeviation < bestAverageDeviation){
+                bestSlope = testLine.slope;
+                bestYIntercept = testLine.yIntercept;
+                bestAverageDeviation = testLine.averageDeviation;
+                System.out.println("---------------------");
+
+                System.out.println(bestAverageDeviation);
+            }
+        }
+
+        return new Line(bestSlope, bestYIntercept);
+
+    }
+
 
     @Override
     public void run() {
@@ -60,68 +77,6 @@ public class Main implements Runnable {
             render();
             pause(60);
         }
-    }
-
-    /**
-     *
-     * @param errorPoints ArrayList of error points with (+/-) values
-     * @param iterations Number of iterations
-     * @return Fitted line
-     */
-    public Line calculateFittedLine(ArrayList<Point> errorPoints, int iterations){
-
-        double lowestSlope = 0;
-        double lowestY_Intercept = 0;
-        double lowestDeviation = 0;
-
-        for (int i = 0; i < iterations; i++){
-
-            // Select two random points from error points
-            Point a = errorPoints.get((int)(Math.random()*errorPoints.size()));
-            Point b = errorPoints.get((int)(Math.random()*errorPoints.size()));
-
-            // Get slope and y-intercept from the two error points
-            Line error_line = calculateEquation(a, b);
-
-            // Store first line's average deviation for future comparisons
-            if (i == 0){
-                error_line.calculateAverageDeviation(errorPoints);
-                lowestDeviation = error_line.averageDeviation;
-                continue;
-            }
-
-            // Calculate average deviation of the two error points
-            error_line.calculateAverageDeviation(errorPoints);
-
-            if (error_line.averageDeviation < lowestDeviation){
-
-                System.out.println("––––––");
-                System.out.println("[" + i + "] " + "Ave Dev: " + error_line.averageDeviation);
-                System.out.println("–" + "Slope " + error_line.slope);
-                System.out.println("–" + "Y-Intercept " + error_line.yIntercept);
-
-                lowestSlope = error_line.slope;
-                lowestY_Intercept = error_line.yIntercept;
-                lowestDeviation = error_line.averageDeviation;
-            }
-        }
-        return new Line(lowestSlope, lowestY_Intercept);
-    }
-
-
-
-    /**
-     *
-     * @param a Point a with (x, y)
-     * @param b Point b with (x, y)
-     * @return New line with given points
-     */
-    public Line calculateEquation(Point a, Point b){
-
-        double slope = (a.y - b.y) / (a.x - b.x);
-        double y_intercept = a.y - (a.x * slope);
-
-        return new Line(slope, y_intercept);
     }
 
     private void setUpGraphics(){
@@ -160,8 +115,8 @@ public class Main implements Runnable {
         g.drawLine(screenWidth / 2, 0, screenHeight / 2, screenHeight);
 
 
-        line.drawPoints(this, g, Color.lightGray);
-        line.drawErrorPoints(this, g, Color.orange);
+        modelLine.drawPoints(this, g, Color.lightGray);
+        modelLine.drawErrorPoints(this, g, Color.orange);
 
         initialLine.drawPoints(this, g, Color.red);
 
@@ -179,5 +134,52 @@ public class Main implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     *
+     * @param errorPoints ArrayList of error points with (+/-) values
+     * @param iterations Number of iterations
+     * @return Fitted line
+     */
+    @Deprecated
+    public Line calculateFittedLineDeprecated(ArrayList<Point> errorPoints, int iterations){
+
+        double lowestSlope = 0;
+        double lowestY_Intercept = 0;
+        double lowestDeviation = 0;
+
+        for (int i = 0; i < iterations; i++){
+
+            // Select two random points from error points
+            Point a = errorPoints.get((int)(Math.random()*errorPoints.size()));
+            Point b = errorPoints.get((int)(Math.random()*errorPoints.size()));
+
+            // Get slope and y-intercept from the two error points
+            Line error_line = new Line(a, b);
+
+            // Store first line's average deviation for future comparisons
+            if (i == 0){
+                error_line.calculateAverageDeviation(errorPoints);
+                lowestDeviation = error_line.averageDeviation;
+                continue;
+            }
+
+            // Calculate average deviation of the two error points
+            error_line.calculateAverageDeviation(errorPoints);
+
+            if (error_line.averageDeviation < lowestDeviation){
+
+                System.out.println("––––––");
+                System.out.println("[" + i + "] " + "Ave Dev: " + error_line.averageDeviation);
+                System.out.println("–" + "Slope " + error_line.slope);
+                System.out.println("–" + "Y-Intercept " + error_line.yIntercept);
+
+                lowestSlope = error_line.slope;
+                lowestY_Intercept = error_line.yIntercept;
+                lowestDeviation = error_line.averageDeviation;
+            }
+        }
+        return new Line(lowestSlope, lowestY_Intercept);
     }
 }
